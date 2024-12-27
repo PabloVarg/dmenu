b287430629a426e672658ad56eae6f4c502aba8d
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-27 17:39:07 -0600

    style(colors): Implement gruvbox material

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xca;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xf0;     /* Amount of opacity. 0xff is opaque             */
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
@@ -13,8 +13,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#EBDBB2", "#1d2021" },
-	[SchemeSel] = { "#EBDBB2", "#665C54" },
+	[SchemeNorm] = { "#d4be98", "#1d2021" },
+	[SchemeSel] = { "#d4be98", "#0d3138" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
