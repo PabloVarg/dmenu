9865b59710267061f46a8501d893bd12d2c1e2ba
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-27 17:26:38 -0600

    feat(patches): Downlad border patch

--- patches/urls.sh
+++ patches/urls.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 curl -O https://tools.suckless.org/dmenu/patches/alpha/dmenu-alpha-20230110-5.2.diff
-curl -O https://tools.suckless.org/dmenu/patches/border/dmenu-border-20230512-0fe460d.diff
+curl -O https://tools.suckless.org/dmenu/patches/border/dmenu-border-5.2.diff 
 curl -O https://tools.suckless.org/dmenu/patches/center/dmenu-center-5.2.diff
 curl -O https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-5.3.diff
